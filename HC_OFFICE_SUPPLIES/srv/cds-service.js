8488808ac268f3aa446d92d45eea9d7c64f18076
robertrivero <robert.rivero@sybven.com>
2023-06-09 16:14:18 +0000

    Localization Commit

--- HC_OFFICE_SUPPLIES/srv/cds-service.js
+++ HC_OFFICE_SUPPLIES/srv/cds-service.js
@@ -13,4 +13,20 @@ module.exports = cds.service.impl(function () {
             this.emit('prod_Change', header)
         })
     })
+
+    this.on('get_supplier_info', async () => {
+        try{
+            const db = await cds.connect.to('db')
+            const dbClass = require("sap-hdbext-promisfied")
+            let dbConn = new dbClass(await dbClass.createConnection(db.operations.credentials))
+            const hdbext = require("@sap/hdbext")
+            const sp = await dbConn.loadProcedurePromisified(hdbext, null, 'get_supplier_info')
+            const output = await dbConn.callProcedurePromisified(sp, [])
+            console.log(output.results)
+            return output.results
+        } catch {
+            console.error(error)
+            return
+        }
+    })
 })
